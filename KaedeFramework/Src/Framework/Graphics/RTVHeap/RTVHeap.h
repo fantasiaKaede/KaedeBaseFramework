d695f71b3bb1d76c271db4e48da0d22dba0b2f62
Kaede Ueda <kaedeueda29@gmail.com>
2025-10-24 15:55:15 +0900

    RTVヒープクラス中身実装
    次はグラフィックスでユニークポインタでRTVヒープクラスを宣言し実装する

--- KaedeFramework/Src/Framework/Graphics/RTVHeap/RTVHeap.h
+++ KaedeFramework/Src/Framework/Graphics/RTVHeap/RTVHeap.h
@@ -4,8 +4,20 @@ class RTVHeap
 public:
 	RTVHeap(){}
 	~RTVHeap(){}
-
+	//ヒープ作成
+	//成功したらtrue
+	bool Create(ID3D12Device* pDevice, int useCount);
+	//RTV作成
+	//バッファーのポインタ
+	//ヒープの紐付けられた登録番号
+	int CreateRTV(ID3D12Resource* pBuffer);
+	//RTVのCPU側アドレスを返す関数
+	D3D12_CPU_DESCRIPTOR_HANDLE GetRTVCPUHandle(int number);
 private:
-
+	ID3D12Device* m_pDevice = nullptr;
+	ComPtr<ID3D12DescriptorHeap>m_pHeap = nullptr;
+	int m_useCount = 0;
+	int m_incrementSize = 0;
+	int m_nextRegistNumber = 0;
 };
 
